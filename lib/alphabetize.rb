10087f0f7d86bda6d68eab51b532e580669e57e2
Omar A <oazzat19@gmail.com>
2018-10-28 20:02:37 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -6,8 +6,10 @@ def alphabetize(arr)
   arr.each do |string|
     new_array.push(string.split(""))
   end
+  
+  new_array.sort_by do |item|
   new_array.each do |letter|
   letter = ESPERANTO_ALPHABET.index(letter)
 end 
-new_array.sort_by { |stringg| new_array  }
+
 end
