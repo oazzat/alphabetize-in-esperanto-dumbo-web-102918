64507b758917d85458a25d63fc8f17c4ac926fe7
Omar A <oazzat19@gmail.com>
2018-10-28 19:50:36 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,8 +3,8 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   new_array = []
   
-  arr.each do |word|
-    if 
+  arr.each do |string|
+    new_array.push(string.split(""))
   end
   
 end
